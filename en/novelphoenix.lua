c3bce60bf2e24a515827acea2971eb1867e4b18c
Vaizer0 <137601862+Vaizer0@users.noreply.github.com>
2026-06-25 13:02:04 +0530

    Bump version from 1.0.1 to 1.1.0

--- en/novelphoenix.lua
+++ en/novelphoenix.lua
@@ -4,7 +4,7 @@
 
 id       = "novelphoenix"
 name     = "Novel Phoenix"
-version  = "1.0.1"
+version  = "1.1.0"
 baseUrl  = "https://novelphoenix.com"
 language = "en"
 icon     = "https://novelphoenix.com/logo.png"
